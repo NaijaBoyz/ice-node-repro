4e5ed24144f47cda51f0e06e8967ebfbbdeedcca
NaijaBoyz <gbemioluwagbire.123@gmail.com>
2025-11-20 17:46:56 -0500

    added Multi-Level CCS 2015 mapping download with extraction

--- download_dataset.sh
+++ download_dataset.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 
-USER_NAME="ooluwagbire3"
-USER_PASS="ChelseaFC123="
+USER_NAME="username"
+USER_PASS="password"
 
 M3_BASE_URL="https://physionet.org/files/mimiciii/1.4"
 M3_DEST="./data/mimiciii-1.4"
@@ -24,18 +24,27 @@ M4_FILES=(
 )
 
 MAPPINGS_DEST="./data/mappings"
+MULTI_CCS_DEST="$MAPPINGS_DEST/Multi_Level_CCS_2015"
+MULTI_CCS_URL="https://hcup-us.ahrq.gov/toolssoftware/ccs/Multi_Level_CCS_2015.zip"
 
 mkdir -p "$M3_DEST"
 mkdir -p "$M4_DEST"
 mkdir -p "$MAPPINGS_DEST"
+mkdir -p "$MULTI_CCS_DEST"
 
 echo "Downloading mapping files..."
 wget -O "$MAPPINGS_DEST/CCS_ICD9_MultiLevelDx.txt" \
-     "https://hcup-us.ahrq.gov/toolssoftware/ccs/AppendixCMultiDX.txt"
+     "https://hcup-us.ahrq.gov/toolssoftware/ccs/AppendixCMultiDX.txt" # TODO: Update this and one below 
 wget -O "$MAPPINGS_DEST/CCS_CategoryNames_FullLabels.pdf" \
-     "https://hcup-us.ahrq.gov/toolssoftware/ccs/CCSCategoryNames_FullLabels.pdf"
+     "https://hcup-us.ahrq.gov/toolssoftware/ccs/CCSCategoryNames_FullLabels.pdf" # TODO: Update this and one above
 wget -O "$MAPPINGS_DEST/icd10cm_to_icd9cm_gem.csv" \
      "https://data.nber.org/gem/icd10cmtoicd9gem.csv"
+
+echo "Downloading Multi-Level CCS ZIP..."
+wget --continue --no-parent --directory-prefix="$MULTI_CCS_DEST" "$MULTI_CCS_URL"
+
+echo "Unzipping Multi-Level CCS..."
+unzip -o "$MULTI_CCS_DEST/Multi_Level_CCS_2015.zip" -d "$MULTI_CCS_DEST"
 
 echo "Downloading MIMIC-III files..."
 for f in "${M3_FILES[@]}"; do
